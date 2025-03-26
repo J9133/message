b9bfb5ea3686bf5ed8420d26134418013b2d60cf
J9133 <jad.salim.13.2011@gmail.com>
2025-03-26 23:42:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+import {update, hide, get} from './func.js';
+const In =document.getElementById('In')
+let messagenum = 0
+const url = "https://api.github.com/repos/J9133/messageData/contents/data.json";
+const url2 = "https://api.github.com/repos/J9133/messageData/contents/datanum.json";
+const token = "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";
+
+async function sand() {
+  if (In.value) {
+    const datanum = await get(url2, token);
+    let num = JSON.parse(datanum).num;
+    let key = "message" + num;
+    update(url, token, { [key]: In.value });
+    update(url2, token, { num: num + 1
+    });
+    console.log(key);
+    In.value = ''
+    
+  }
+}
+
+async function reset() {
+  await update(url, token, {}); 
+  await update(url2, token, { num: 0 }); 
+  document.querySelectorAll('#message').forEach(element => element.remove());
+  messagenum = 0;
+}
+
+async function main() {
+  try {
+    const datanum = await get(url2, token)
+    let num = JSON.parse(datanum).
+    num
+    messagenum = num
+    
+    const messages = document.querySelectorAll('#message');
+    const d = await get(url, token);
+    const jsondata = JSON.parse(d);
+    const data = Object.values(jsondata);
+    console.log(data);
+    
+    messages.forEach(element => element.remove());
+    
+    for (let i = 0; i < data.length; i++) {
+      hide(data[i]);
+    }
+  } catch (error) {
+    console.error("حدث خطأ:", error);
+  }
+}
+main()
+
+setInterval(main, 1000)
+
+window.sand = sand
+window.reset = reset
